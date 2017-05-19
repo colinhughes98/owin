e7ec28dc145b97a5d0bbbe96aede95bd0bf75641
Colin Hughes <colinhughes@gmail.com>
2017-05-19 13:54:16 +0100

    first commit

---
+++ owin_test/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(owin_test.Startup))]
+
+namespace owin_test
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            //app.Use<ColMiddleWare>();
+            app.UseColMiddleWare();
+            app.Use<TeapotMiddleware>();
+            
+        }
+    }
+}
---
+++ owin_test/Teapot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string,object>,System.Threading.Tasks.Task>;
+namespace owin_test
+{
+    public class TeapotMiddleware : OwinMiddleware
+    {
+        public TeapotMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            context.Response.StatusCode = 418;
+            // await this.Next.Invoke(context);
+        }
+    }
+
+    public class ColMiddleWare
+    {
+        private readonly AppFunc _next;
+
+        public ColMiddleWare(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            Debug.WriteLine("start col middleware");
+            await _next.Invoke(env);
+            Debug.WriteLine("end col middleware");
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void UseColMiddleWare(this IAppBuilder app)
+        {
+            app.Use<ColMiddleWare>();
+        }
+    }
+}
